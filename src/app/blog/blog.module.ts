c4eb4382f2fc8f0b473fb1b801e49d957e029236
Dani <daniru@gmail.com>
2017-03-02 22:43:36 +0000

    add blog module

---
+++ src/app/blog/blog.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlogRoutingModule } from './blog-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BlogRoutingModule
+  ],
+  declarations: []
+})
+export class BlogModule { }
