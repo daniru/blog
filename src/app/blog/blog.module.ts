06dd778b927361172700ef9bb01f3f4dafcaf3c1
Dani <daniru@gmail.com>
2017-03-02 22:45:16 +0000

    providing blog service in the blog module

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
+import { BlogService } from './services/blog.service';
 
 @NgModule({
   imports: [
     CommonModule,
     BlogRoutingModule
   ],
+  providers: [BlogService],
   declarations: []
 })
 export class BlogModule { }
