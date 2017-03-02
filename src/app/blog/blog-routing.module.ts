aa4d93e96f0d08497f47e1063ed0294188917ce3
Dani <daniru@gmail.com>
2017-03-02 22:49:30 +0000

    Link all together

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
+import { ItemComponent } from './components/item/item.component';
 
 const routes: Routes = [
-  { path: '', component: ListComponent }
+  { path: '', component: ListComponent },
+  { path: ':key', component: ItemComponent }
 ];
 
 @NgModule({
