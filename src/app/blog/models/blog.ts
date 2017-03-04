3bd9c7a56fef9ad385abf27b36dafc69cba874e3
Dani <daniru@gmail.com>
2017-03-04 11:58:58 +0000

    extend interfaces #2

--- src/app/blog/models/blog.ts
+++ src/app/blog/models/blog.ts
@@ -1,4 +1,10 @@
+import { Section } from './section';
+
 export interface Blog {
-    key: string;
-    title: string;
+  key: string;
+  header: string | null;
+  title: string;
+  date_created: Date;
+  date_published: Date | null;
+  sections: Section[];
 }
