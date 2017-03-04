018b1f6545a89b5516ce85bad2c4790201624960
Dani <daniru@gmail.com>
2017-03-04 10:35:19 +0000

    adding material to the project and header #4

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule} from '@angular/router';
+import { MdToolbarModule } from '@angular/material';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MdToolbarModule
   ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
